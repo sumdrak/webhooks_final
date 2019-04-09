1a67b03c4908b37146fff4cbd8e4e70a258b6839
Andres esteban chavez <sumdrak@gmail.com>
2019-04-09 16:29:27 -0500

    segundo commit

---
+++ script.php
@@ -0,0 +1,27 @@
+
+
+ <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2082109285177337',
+      xfbml      : true,
+      version    : 'v3.2'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
