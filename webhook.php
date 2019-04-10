4b360262419a00049b3e0e0fec0067f8d7bf3e77
Andres esteban chavez <sumdrak@gmail.com>
2019-04-10 10:04:28 -0500

    tercer commit

--- webhook.php
+++ webhook.php
@@ -11,6 +11,9 @@ if(isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub
     }
 }else{
     $feedData = file_get_contents('php://input');
+    if(empty($feedData)){
+        $feedData = "no tiene informacion";
+    }
     /* echo ($feedData);*/
     $handle = fopen('test.txt','w');
     fwrite($handle,$feedData);
@@ -22,7 +25,7 @@ if(isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub
         $commentID = $data->entry[0]->changes[0]->value->comment_id;
         $message = $data->entry[0]->changes[0]->value->message;
         $verb = $data->entry[0]->changes[0]->value->verb;
-        $accessToken = "EAAdlqthTlZCkBAFuVsg0awBE5pegZBFv5oWwWUQcQi6iRKMNJRftfPOkNI0ML72mHiy1p2fZCafxq6PtqDj3ZCEO1sdsxRoINZB1I1hZA9VSxSu8MnzNBvnjGMw5R2tKVykI7BgZCZCrQZACz8kjpvEDsH24ZBLpUzKuOoKgZAvxTkNEAZDZD";
+        $accessToken = "EAAdlqthTlZCkBANl52dydPeozRYzwj10AzVaZATiOt9R7ZB01Lcd61u1zkCJ2l5JNjZBgsPOKpHEjnL9fcWJoV76I4vYBnjTeNeNOfgZCIhTDTSxOQq2ZB4CgiDsAit95qc67pTIaPaE999LyHzOgY6ZB8OHYQaWynjEKqCdT4WqoYQa4npwy6iw7PwJHnkYOQZD";
         if($verb == "add"){
             if(strtolower($message) == "red"){
                 $reply = "hey, noi se";
@@ -31,7 +34,7 @@ if(isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub
             }else{
                 $reply = "hey, noi se 3";
             }
-            $ch = curl_init();
+            /* $ch = curl_init();
             curl_setopt($ch,CURLOPT_POST,1);
             curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
             curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
@@ -39,7 +42,7 @@ if(isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub
             curl_setopt($ch,CURLOPT_POSTFIELDS,"message=$reply&access_token=$accessToken");
             curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
             $response = curl_exec($ch);
-            curl_close($ch);
+            curl_close($ch); */
         }
     }
     }else{
