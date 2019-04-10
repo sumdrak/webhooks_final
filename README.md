00d93df1fbb9fb769aa7178c7dd2d52e5dad8e9e
Andres esteban chavez <sumdrak@gmail.com>
2019-04-10 10:22:49 -0500

    cuarto commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Mautic-facebook leads
+Use Facebook Webhook and Mautic API to automatically create contact in Mautic when a Lead is generated via Facebook ads.
+
+Use platform.php to subscribe to page where you started your facebook lead generation ad.
+Install Mautic API Library: https://github.com/mautic/api-library
+Enable and create API credential in Mautic 
+
+This was a quick work i didn't validate nor optmize all data and variables.
+
+Here is a video tutorial explaining how to use this files: https://youtu.be/Jr98BOamcGo
+
+I created a new video showing how to Integrate Mautic with facebook leads using Mautic Forms : https://youtu.be/mj4ZYrEjx5c
---
+++ webhook-forms.php
@@ -0,0 +1,58 @@
+<?php
+// facebook variables 
+$challenge = isset($_REQUEST['hub_challenge']) ? $_REQUEST['hub_challenge'] : '';
+$verify_token = isset($_REQUEST['hub_verify_token']) ? $_REQUEST['hub_verify_token'] : ''; 
+$fb_access_token = "EAAdlqthTlZCkBANl52dydPeozRYzwj10AzVaZATiOt9R7ZB01Lcd61u1zkCJ2l5JNjZBgsPOKpHEjnL9fcWJoV76I4vYBnjTeNeNOfgZCIhTDTSxOQq2ZB4CgiDsAit95qc67pTIaPaE999LyHzOgY6ZB8OHYQaWynjEKqCdT4WqoYQa4npwy6iw7PwJHnkYOQZD";
+
+// this is used to subscribe to facebook webhook
+if ($verify_token === "verificar_123") { // 
+  echo $challenge;
+}
+
+// Process retrieved data from facebook webhook
+$data = json_decode(file_get_contents("php://input"),true);
+$leadgen_id = $data['entry'][0]['changes'][0]['value']['leadgen_id']; // extract leadgen ID
+
+if($leadgen_id){
+  $ch = curl_init();
+  $url = "https://graph.facebook.com/v2.8/".$leadgen_id;
+  console.log($leadgen_id);
+  $url_query = "access_token=".$fb_access_token; 
+  $url_final = $url.'?'.$url_query;
+  console.log($url_final);
+  curl_setopt($ch, CURLOPT_URL, $url_final);
+  curl_setopt($ch, CURLOPT_HTTPGET, 1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $response = curl_exec($ch);
+  curl_close ($ch);
+  $data = json_decode($response, true);
+  $lead_first = $data['field_data'][2][values][0];
+  $lead_last = $data['field_data'][1][values][0];
+  $lead_email = $data['field_data'][0][values][0];
+} 
+
+// check if we have a valid lead before calling Mautic Api
+if($lead_email){
+  //Create a new Contact
+  $data =array(); 
+  $data['first_name'] = $lead_first;
+  $data['last_name'] = $lead_last;
+  $data['email'] = $lead_email;
+  $data['submit'] = "";
+  $data['formId'] = 3; // change this to the form ID that you created in your Mautic
+  $formId = $data['formId'];
+  $data = array('mauticform' => $data);
+  
+  // Change [path-to-mautic] to URL where your Mautic is
+  /* $formUrl =  '[path-to-mautic]/form/submit?formId=' . $formId;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $formUrl);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($ch);
+  curl_close($ch);
+  return $response; */
+  echo "Contact Added!";
+} // end valid lead check
+?>
